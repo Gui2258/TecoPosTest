147098849af4ca67f30bc436b2a46f9f4bba15b0
Guido Morffi <guidotele@gmail.com>
2025-07-31 10:40:03 -0400

    Creado navegación bar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,34 @@
-import { Stack } from 'expo-router';
+import ThemeToggleButton from '@/src/components/ToggleThemeButton';
+import { useTheme } from '@/src/context/ThemeContext';
+import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
-    return <Stack />;
+    const { colors } = useTheme();
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: colors.navBarIconActive,
+                tabBarInactiveTintColor: colors.navBarIcon,
+                headerStyle: {
+                    backgroundColor: colors.navBarBackground,
+                },
+
+                headerShadowVisible: false,
+                headerTintColor: colors.textPrimary,
+
+                tabBarStyle: {
+                    backgroundColor: colors.navBarBackground,
+                    height: '8%',
+                    paddingTop: 10,
+
+                    // Asegurar que la tab bar esté en la parte inferior
+                    /*  position: 'absolute',
+                        bottom: 0,
+                        left: 0,
+                        right: 0, */
+                },
+                headerRight: () => <ThemeToggleButton />,
+            }}
+        ></Tabs>
+    );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,15 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '../context/ThemeContext';
 
 export default function RootLayout() {
     return (
         <ThemeProvider>
-            <StatusBar
+            {/*  <StatusBar
                 style="inverted"
                 hidden={false} // Mostrar status bar pero ocultar navigation bar
-            />
+            /> */}
             <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" />
             </Stack>
         </ThemeProvider>
     );

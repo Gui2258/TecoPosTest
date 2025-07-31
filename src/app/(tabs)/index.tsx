d58ebb95aeba820dcf21267ec3345909e89fa81c
Guido Morffi <guidotele@gmail.com>
2025-07-31 11:23:10 -0400

    Añadido app icon

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,15 @@
 import { useTheme } from '@/src/context/ThemeContext';
-import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import React, { useCallback, useMemo, useState } from 'react';
+import {
+    Dimensions,
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
 interface DenominationInput {
     value: number;
     quantity: string;
@@ -17,25 +26,37 @@ export default function HomeScreen() {
         { value: 20, quantity: '0', total: 0 },
         { value: 10, quantity: '0', total: 0 },
         { value: 5, quantity: '0', total: 0 },
-        { value: 2, quantity: '0', total: 0 },
         { value: 1, quantity: '0', total: 0 },
     ]);
-    const handleQuantityChange = (text: string, index: number) => {
-        const newDenominations = [...denominations];
-        const quantity = text === '' ? '0' : text;
-        newDenominations[index] = {
-            ...newDenominations[index],
-            quantity,
-            total: newDenominations[index].value * parseInt(quantity || '0'),
-        };
-        setDenominations(newDenominations);
-    };
-    const totalSum = denominations.reduce((acc, curr) => acc + curr.total, 0);
+    const handleQuantityChange = useCallback((text: string, index: number) => {
+        const numericText = text.replace(/[^0-9]/g, '');
+
+        setDenominations((prev) => {
+            const newDenominations = [...prev];
+            newDenominations[index] = {
+                ...newDenominations[index],
+                quantity: numericText,
+                total:
+                    newDenominations[index].value *
+                    (numericText ? parseInt(numericText, 10) : 0),
+            };
+            return newDenominations;
+        });
+    }, []);
+    const totalSum = useMemo(
+        () => denominations.reduce((acc, curr) => acc + curr.total, 0),
+        [denominations]
+    );
+    const { width } = Dimensions.get('screen');
     const styles = StyleSheet.create({
         container: {
             flex: 1,
             backgroundColor: colors.background,
             padding: 16,
+        },
+        scrollContent: {
+            padding: 16,
+            flexGrow: 1,
         },
         row: {
             flexDirection: 'row',
@@ -78,9 +99,47 @@ export default function HomeScreen() {
             backgroundColor: colors.surface,
             borderRadius: 8,
         },
+        button: {
+            backgroundColor: colors.primary,
+            padding: 10,
+            borderRadius: 12,
+            marginBottom: 20,
+            width: width * 0.2,
+            alignItems: 'center',
+            justifyContent: 'center',
+            shadowColor: '#000',
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+            borderWidth: 1,
+            borderColor: colors.primary,
+        },
+        text: {
+            color: colors.background,
+            fontSize: 20,
+            justifyContent: 'center',
+            textAlign: 'center',
+        },
     });
+
+    const clearData = () => {
+        const newDenominations = denominations.map((item) => ({
+            ...item,
+            quantity: '0',
+            total: 0,
+        }));
+        setDenominations(newDenominations);
+    };
+
     return (
-        <ScrollView style={styles.container}>
+        <ScrollView
+            contentContainerStyle={styles.scrollContent}
+            keyboardShouldPersistTaps="handled"
+        >
             {denominations.map((item, index) => (
                 <View key={item.value} style={styles.row}>
                     <Text style={styles.denomination}>${item.value}</Text>
@@ -98,6 +157,11 @@ export default function HomeScreen() {
                 </View>
             ))}
             <Text style={styles.grandTotal}>Total: ${totalSum}</Text>
+            <Pressable onPress={clearData}>
+                <View style={styles.button}>
+                    <Feather name="trash-2" size={30} color="white" />
+                </View>
+            </Pressable>
         </ScrollView>
     );
 }

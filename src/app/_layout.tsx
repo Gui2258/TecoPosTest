9dee01ba7bf224fbda91d9fbfc61f6ebdd09aed2
Guido Morffi <guidotele@gmail.com>
2025-07-31 10:30:44 -0400

    Añadida vista de tabs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,17 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { ThemeProvider } from "../context/ThemeContext";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from '../context/ThemeContext';
 
 export default function RootLayout() {
-  return (
-    <ThemeProvider>
-      <StatusBar
-        style="inverted"
-        hidden={false} // Mostrar status bar pero ocultar navigation bar
-      />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <StatusBar
+                style="inverted"
+                hidden={false} // Mostrar status bar pero ocultar navigation bar
+            />
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </ThemeProvider>
+    );
 }
---
+++ src/components/ToggleThemeButton.tsx
@@ -0,0 +1,31 @@
+// src/components/ThemeToggleButton.tsx
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useTheme } from '../context/ThemeContext'; // Tu hook de tema personalizado
+
+export default function ThemeToggleButton() {
+    const { currentThemeName, toggleTheme, colors } = useTheme();
+
+    return (
+        <TouchableOpacity
+            onPress={toggleTheme}
+            style={{ paddingHorizontal: 12, marginHorizontal: 10 }}
+            accessibilityLabel="Cambiar tema claro/oscuro"
+        >
+            {currentThemeName === 'light' ? (
+                <MaterialCommunityIcons
+                    name="weather-night"
+                    size={24}
+                    color={colors.primary}
+                />
+            ) : (
+                <MaterialCommunityIcons
+                    name="white-balance-sunny"
+                    size={24}
+                    color="#FFD700"
+                />
+            )}
+        </TouchableOpacity>
+    );
+}
